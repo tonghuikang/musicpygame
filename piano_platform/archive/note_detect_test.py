add48f36bea8295c4074156dbd6d581e811b0b33
Tong Hui Kang <tonghuikang@outlook.com>
2018-03-05 07:02:20 +0800

    Rather inaccurate
    I think need to to rewrite this - I will do it something.
    
    Also need to optimise - if D4+ and D4- are peaks (that means D4= isn't a peak), we can drop the consideration of D4 as a note.

--- piano_platform/archive/note_detect_test.py
+++ piano_platform/archive/note_detect_test.py
@@ -1,17 +1,18 @@
 import pyaudio
 import argparse
 
-import cmath
-import math
 import numpy as np
 import time
 
+
+# this is the size of each individual block of audio, we call this "chunk"
 chunksize = 2048
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-input", required=False, type=int, help="Audio Input Device")
 args = parser.parse_args()
 
+# this is here so you can choose the default audio input
 if args.input is None:
     print("No input device specified. Printing list of input devices now: ")
     p = pyaudio.PyAudio()
@@ -20,20 +21,31 @@ if args.input is None:
         print("Run this program with -input 1, or the number of the input you'd like to use.")
         exit()
 
+# starting audio channel?
 p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paFloat32,
 channels=1, rate=44100, input=True,
 input_device_index=args.input, frames_per_buffer=chunksize)
 time.sleep(1)
 
-
-# to be called at the end of every 2048 entries
-# the input is 2048 entry * 3
 def detect_onset(signal, chunksize=2048, tempo_res=32):
-    onset = -1
+    '''
+    # this is to be called at the end of every chunk (2048 entries), starting from 4th chunk
+    # the input to this function is four chunks
+    '''
+    onset = -1 # default, overwritten if onset is detected
+
+    # to detect difference
     difference = np.cumsum(np.add(np.absolute(signal[chunksize:-chunksize]), -np.absolute(signal[:-2*chunksize])))
+    
+    # noise is added to desensitise detection of noise
     noise = 10*np.array(np.random.randn(len(difference)))
     difference = np.add(difference, noise)
+
+    # calculation of r-coefficient
+    # -1 is negatively correlated
+    # +1 is positively correlated
+    # onset is when r-coefficient cuts above 0.8
     roceff = np.full(tempo_res, 0.)
     tempo_num = int(chunksize / tempo_res)
     for i in range(tempo_res):
@@ -46,33 +58,41 @@ def detect_onset(signal, chunksize=2048, tempo_res=32):
     # print(np.max(roceff))
     return onset # none, or a value
             
-def cqt_function(signal_to_ayse): # input in 4096 entries long
+def cqt_function(signal_to_ayse): 
+    # input to this function is 4096 entries long
     length = len(signal_to_ayse)
     print(length)
 
+    # fast fourier transform
     freq_domain = np.fft.fft(signal_to_ayse)
 
+    # defining the 36 notes bins
     bins = 36
-    freq_ref_notes = [261.625565 * (2. ** (n/36. + 4./72.)) for n in range(bins)]
+    freq_ref_notes = [261.625565 * (2. ** (n/36. - 1./72.)) for n in range(bins)]
 
+    # defining the time kernel
     bell_curves = []
     for note in range(len(freq_ref_notes)):
         bell_curve = np.exp(-((np.arange(-1.,1.,2./length))*(2. ** (note/36.)))**2.)
         bell_curves.append(bell_curve) 
 
+    # what was this for?
     kernels = []
     for note in range(36):
         kernels.append(np.multiply(np.exp((np.arange(length)-length/2.)*-1.j*2.*np.pi*freq_ref_notes[note]/44100.)
                                   ,bell_curves[note]  # windowing
                             ))
 
+    # fourier transform for the frequency kernel
     fft_kernels = []
     for note in range(bins):
         fft_kernels.append(np.fft.fft(kernels[note]))
 
+    # creating arrays to populate for each one-third-semitone
     cqt_resp_specs = []
     cqt_resp = []
 
+    # populating the arrays
     for note in range(bins):
         cqt_resp_spec = []
         for entry in range(length):
@@ -80,8 +100,8 @@ def cqt_function(signal_to_ayse): # input in 4096 entries long
         cqt_resp_specs.append(cqt_resp_spec)
         cqt_resp.append(sum([abs(x) for x in cqt_resp_spec]))
 
+    # finding peaks in the cqt response
     notesrum = cqt_resp
-
     notesrum_peak_only = [0.0]*len(notesrum)
     notesrum_sum = sum(notesrum)
 
@@ -89,13 +109,15 @@ def cqt_function(signal_to_ayse): # input in 4096 entries long
         if notesrum[index-1] < notesrum[index] and notesrum[index+1] < notesrum[index]:
             notesrum_peak_only[index]=notesrum[index]
 
-    # known_octave = notesrum_peak_only[12:12+36]
+    # known_octave = notesrum_peak_only[12:12+36] # don't know what is this for
     known_octave = notesrum_peak_only[:]
+    print(np.round(known_octave,5)/notesrum_sum)
     
     notesrum_peak_only_sum = sum(notesrum_peak_only)
 
     for x in range(bins):
-        if known_octave[x]/notesrum_sum < 0.1: 
+        # if known_octave[x]/notesrum_sum < 0.1: 
+        if known_octave[x]/notesrum_peak_only_sum < 0.2: 
             known_octave[x] = 0
         
     known_octave_notes = []
@@ -105,7 +127,8 @@ def cqt_function(signal_to_ayse): # input in 4096 entries long
                              +known_octave[3*notes+2])
 
     # notestrum_sum = sum(notesrum)  # alternate demoninator to calc threshold
-    print(np.round(known_octave_notes,2))
+    print(np.round(known_octave_notes,5)/notesrum_peak_only_sum)
+    print("check")
 
     notesrum_peak_only_sum = sum(notesrum_peak_only)
 
@@ -124,6 +147,7 @@ def note_detect(chunksize=2048, tempo_res=32):
 
     while True:
         # assume pyaudio clip mono sound
+        # use assert?
         
         i += 1
         data = stream.read(chunksize, exception_on_overflow=False)
@@ -182,6 +206,7 @@ def note_detect(chunksize=2048, tempo_res=32):
                     if output[i] == 12:
                         output[i] = "B"
                 print(output)    
-
+    
+    # don't you need to return something?
 
 note_detect()
